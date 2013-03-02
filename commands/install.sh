daa502e7548c17e5406415a3f1f450bdbff31f18
Flavius Aspra <flavius.as@gmail.com>
2013-03-02 19:02:26 +0100

    timing for install

--- commands/install.sh
+++ commands/install.sh
@@ -6,31 +6,50 @@ PHP_PATCHVER=`echo ${PHP_VERSION##*.}`
 PHP_BRANCH="php-$PHP_MAJORVER.$PHP_MINORVER.$PHP_PATCHVER"
 
 
-msg_info "Building prerequisites"
-cmd "$MULTIVER_ROOT/build-requirements/$PHP_VERSION/build.sh"
-export PATH="$MULTIVER_ROOT/built-requirements/$PHP_VERSION/bin":$PATH
+msg_info "Building bison 2.3"
+source "$MULTIVER_ROOT/commands/bison.sh"
+#TODO bison versions
+export PATH="$MULTIVER_ROOT/build-requirements/$BISON_VER/bin":$PATH
 
+start=`now`
 msg_info "Preparing PHP"
 pushd "$MULTIVER_ROOT/php-src"
 cmd "git checkout $PHP_BRANCH"
 cmd "./buildconf --copy --force"
 cmd "./genfiles"
+DURATION=$(( $(now) - $start ))
+msg_info "Preparing PHP took $DURATION seconds"
 popd
 
+start=`now`
 msg_info "Configuring PHP"
 cmd "mkdir -p $MULTIVER_ROOT/build/$PHP_VERSION"
 pushd "$MULTIVER_ROOT/build/$PHP_VERSION"
-cmd "$MULTIVER_ROOT/php-src/configure --prefix=$MULTIVER_ROOT/inst/$PHP_VERSION ${@:2}"
+cmd "$MULTIVER_ROOT/php-src/configure --prefix=$MULTIVER_ROOT/inst/$PHP_VERSION $EXTENDEDPARAMS"
+DURATION=$(( $(now) - $start ))
+msg_info "Configuring PHP took $DURATION seconds"
+
+start=`now`
 msg_info "Building PHP"
 cmd "make"
+DURATION=$(( $(now) - $start ))
+msg_info "Making PHP took $DURATION seconds"
+
+start=`now`
 msg_info "Installing PHP"
 cmd "make install"
+DURATION=$(( $(now) - $start ))
+msg_info "Installing PHP took $DURATION seconds"
+
 popd
 
+start=`now`
 msg_info "Cleaning up PHP source"
 pushd "$MULTIVER_ROOT/php-src"
 cmd "git clean -fdx"
 cmd "git reset --hard master"
 cmd "git checkout master"
 popd
+DURATION=$(( $(now) - $start ))
+msg_info "Cleaning the PHP source took $DURATION seconds"
 
